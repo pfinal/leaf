97718df5f4208c8a3673bfc292605e2f76908a4d
ethan <nicesoft333@qq.com>
2017-08-10 12:24:09 +0800

    cachePath

--- src/Provider/CacheProvider.php
+++ src/Provider/CacheProvider.php
@@ -16,7 +16,7 @@ class CacheProvider implements ServiceProviderInterface
     {
         $app['cache'] = function () use ($app) {
             $config = isset($app['cache.config']) ? $app['cache.config'] : array();
-            $config += array('class' => 'PFinal\Cache\FileCache');
+            $config += array('class' => 'PFinal\Cache\FileCache', 'cachePath' => $app->getRuntimePath() . DIRECTORY_SEPARATOR . 'cache');
             $class = $config['class'];
             unset($config['class']);
             return $app->make($class, array('config' => $config));
