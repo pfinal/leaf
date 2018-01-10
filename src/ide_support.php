ef46f7986b78fd8c8bc314f26810fa2e7f6c9a95
ethan <nicesoft333@qq.com>
2018-01-10 10:01:07 +0800

    log

--- src/ide_support.php
+++ src/ide_support.php
@@ -9,6 +9,7 @@ namespace Leaf {
     use PFinal\Cache\FileCache;
     use PFinal\Database\Builder;
     use PFinal\Session\NativeSession;
+    use Psr\Log\LoggerInterface;
 
     class Route
     {
@@ -156,24 +157,124 @@ namespace Leaf {
 
     class Log
     {
-        public static function debug($var)
+        /**
+         * System is unusable.
+         *
+         * @param string $message
+         * @param array $context
+         *
+         * @return void
+         */
+        public static function emergency($message, array $context = array())
         {
-            (new FileTarget())->debug($var);
         }
 
-        public static function error($var)
+        /**
+         * Action must be taken immediately.
+         *
+         * Example: Entire website down, database unavailable, etc. This should
+         * trigger the SMS alerts and wake you up.
+         *
+         * @param string $message
+         * @param array $context
+         *
+         * @return void
+         */
+        public static function alert($message, array $context = array())
         {
-            (new FileTarget())->error($var);
         }
 
-        public static function warning($var)
+        /**
+         * Critical conditions.
+         *
+         * Example: Application component unavailable, unexpected exception.
+         *
+         * @param string $message
+         * @param array $context
+         *
+         * @return void
+         */
+        public static function critical($message, array $context = array())
         {
-            (new FileTarget())->warning($var);
         }
 
-        public static function info($var)
+        /**
+         * Runtime errors that do not require immediate action but should typically
+         * be logged and monitored.
+         *
+         * @param string $message
+         * @param array $context
+         *
+         * @return void
+         */
+        public static function error($message, array $context = array())
         {
-            (new FileTarget())->info($var);
+        }
+
+        /**
+         * Exceptional occurrences that are not errors.
+         *
+         * Example: Use of deprecated APIs, poor use of an API, undesirable things
+         * that are not necessarily wrong.
+         *
+         * @param string $message
+         * @param array $context
+         *
+         * @return void
+         */
+        public static function warning($message, array $context = array())
+        {
+        }
+
+        /**
+         * Normal but significant events.
+         *
+         * @param string $message
+         * @param array $context
+         *
+         * @return void
+         */
+        public static function notice($message, array $context = array())
+        {
+        }
+
+        /**
+         * Interesting events.
+         *
+         * Example: User logs in, SQL logs.
+         *
+         * @param string $message
+         * @param array $context
+         *
+         * @return void
+         */
+        public static function info($message, array $context = array())
+        {
+        }
+
+        /**
+         * Detailed debug information.
+         *
+         * @param string $message
+         * @param array $context
+         *
+         * @return void
+         */
+        public static function debug($message, array $context = array())
+        {
+        }
+
+        /**
+         * Logs with an arbitrary level.
+         *
+         * @param mixed $level
+         * @param string $message
+         * @param array $context
+         *
+         * @return void
+         */
+        public static function log($level, $message, array $context = array())
+        {
         }
     }
 
