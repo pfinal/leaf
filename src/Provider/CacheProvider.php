090c15ecefe55937551eb067f54ef6e7569a41b4
Ethan <nicesoft333@qq.com>
2019-02-14 11:05:28 +0800

    cache

--- src/Provider/CacheProvider.php
+++ src/Provider/CacheProvider.php
@@ -14,12 +14,13 @@ class CacheProvider implements ServiceProviderInterface
      */
     public function register(Container $app)
     {
-        $app['cache'] = function () use ($app) {
+        $app['PFinal\\Cache\\CacheInterface'] = $app['cache'] = function () use ($app) {
             $config = isset($app['cache.config']) ? $app['cache.config'] : array();
             $config += array('class' => 'PFinal\Cache\FileCache', 'cachePath' => $app->getRuntimePath() . DIRECTORY_SEPARATOR . 'cache');
             $class = $config['class'];
             unset($config['class']);
             return $app->make($class, array('config' => $config));
         };
+
     }
 }
